31a372aa584e6dc437f31e8443b7f7932215579b
achetanvarma <chetanvarmaatla@gmail.com>
2025-10-10 21:19:29 +0530

    Adding anpr_video files

--- crud.py
+++ crud.py
@@ -9,33 +9,33 @@ connection = pymysql.connect(
 
 with connection.cursor() as cursor:
         # insert data
-        #insert_query = "INSERT INTO  employee_data(emp_name,emp_role)VALUES(%s,%s)"
-        #values=("JHON","Software engineer")
-        #cursor.execute(insert_query,values)
-        #connection.commit()
+        insert_query = "INSERT INTO  employee_data(emp_name,emp_role)VALUES(%s,%s)"
+        values=("JHON","Software engineer")
+        cursor.execute(insert_query,values)
+        connection.commit()
 
         # read
-        #select_query ="SELECT * FROM employee_data"
-        #cursor.execute(select_query)
-        #rows=cursor.fetchall()
-        #print("\n employes list:")
-        #for row in rows:
-         #       print(row)
+        select_query ="SELECT * FROM employee_data"
+        cursor.execute(select_query)
+        rows=cursor.fetchall()
+        print("\n employes list:")
+        for row in rows:
+               print(row)
 
         #update
-        #update_query="UPDATE employee_data set emp_role=%s WHERE emp_id = %s"
-        #cursor.execute(update_query,("Software engineer",1))
-        #connection.commit()
-        #print("\n employee salary updated!")
+        update_query="UPDATE employee_data set emp_role=%s WHERE emp_id = %s"
+        cursor.execute(update_query,("Software engineer",1))
+        connection.commit()
+        print("\n employee salary updated!")
 
 
         #read 
-        #select_query ="SELECT * FROM employee_data"
-        #cursor.execute(select_query)
-        #rows=cursor.fetchall()
-        #print("\n employes list:")
-        #for row in rows:
-          #      print(row)
+        select_query ="SELECT * FROM employee_data"
+        cursor.execute(select_query)
+        rows=cursor.fetchall()
+        print("\n employes list:")
+        for row in rows:
+               print(row)
 
 
         #delete 
